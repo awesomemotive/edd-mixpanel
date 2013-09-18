f7c9fdc58ff84ceac2fb400806e5a28e70714481
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-18 10:48:08 -0500

    Add price to Add to Cart event

--- edd-mixpanel.php
+++ edd-mixpanel.php
@@ -65,6 +65,7 @@ final class EDD_Mixpanel {
 		$event_props['ip']           = edd_get_ip();
 		$event_props['session_id']   = EDD()->session->get_id();
 		$event_props['product_name'] = get_the_title( $download_id );
+		$event_props['product_price']= edd_get_cart_item_price( $download_id, $options );
 		if( function_exists( 'rcp_get_subscription' ) && is_user_logged_in() ) {
 			$event_props['subscription'] = rcp_get_subscription( get_current_user_id() );
 		}
